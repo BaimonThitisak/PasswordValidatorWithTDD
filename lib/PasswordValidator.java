e353937e625d34dc9b730e9a1d44f32f3e795243
BaimonThitisak <thitisak.siri@ku.th>
2025-07-04 16:17:46 +0700

    Add files via upload

--- lib/PasswordValidator.java
+++ lib/PasswordValidator.java
@@ -5,15 +5,60 @@ public class PasswordValidator {
     /**
      * คุณจะต้องเขียน Javadoc ที่สมบูรณ์ที่นี่ในอนาคต
      * เพื่ออธิบายกฎการทำงานของเมธอด
+     * @param password ที่ต้องการตรวจสอบ
+     * @return ระดับความปลอดภัยของรหัส
      */
     // TODO: แก้ไข return type ของเมธอดนี้ให้ถูกต้อง
-    public static void validate(String password) { // Function Type ให้เป็น PasswordStrength 
+    public static PasswordStrength validate(String password) { // Function Type ให้เป็น PasswordStrength 
         
+        boolean UpperCase = false ;
+        boolean LowerCase = false ;
+        boolean Special = false ;
+        boolean Digit = false ;
+
         // ส่วนของ Implementation ที่คุณต้องเขียน
-        // ...
+        //วนลูปเช็ครหัสผ่านว่ามี ตัวพิมพ์เล็ก, ตัวพิมพ์ใหญ่, ตัวเลข, ตัวพิเศษ มั้ย
+            for (int i = 0 ; i < password.length() ; i++ ) {
+                char c = password.charAt(i);
+                if (Character.isUpperCase(c)) {
+                    UpperCase = true ;
+                }
+                else if (Character.isLowerCase(c)) {
+                    LowerCase = true ;
+                }
+                else if (Character.isDigit(c)) {
+                    Digit = true ;
+                }
+                else Special = true ;
+
+            } 
         
-        int minLength = 8 // TODO: มีอะไรขาดหายไปที่บรรทัดนี้?
-        
-        return null ; // TODO: การคืนค่านี้ถูกต้องหรือไม่?
+            //ตรวจว่าเกิน 8 ตัวอักษรหรือเป็นช่องว่างมั้ย
+            int minLength = 8 ; // TODO: มีอะไรขาดหายไปที่บรรทัดนี้?
+            if (password.length() < minLength || password == null) {
+                return PasswordStrength.INVALID;
+            }
+
+            //ตรวจว่าผ่านกี่อย่าง
+            int passed = 0 ;
+            if (password.length() >= minLength || password == null) { passed ++ ;}
+            if (UpperCase) {passed ++ ;}
+            if (LowerCase) {passed ++ ;}
+            if (Digit) {passed ++ ;}
+            if (Special) {passed ++ ;}
+            
+
+            //กำหนดว่าต้องผ่านกี่อย่างถึงได้ความปลอดภัยเท่าไหร่
+            if (passed >= 4) {
+                return PasswordStrength.STRONG ;
+            }
+            else if (passed >= 3) {
+                return PasswordStrength.MEDIUM ;
+            }
+            else if (passed >= 2 ) {
+                return PasswordStrength.WEAK ;
+            }
+
+        return PasswordStrength.INVALID ; 
     }
 }
--- lib/PasswordValidatorTest.java
+++ lib/PasswordValidatorTest.java
@@ -3,7 +3,7 @@ package lib;
 public class PasswordValidatorTest{
     
     public static void run() {
-        System.out.println("--- Running Password Validator Tests ---");
+        System.out.println("------ Running Password Validator Tests ------");
         
         // Test Case 1: รหัสผ่านสั้นควรจะ INVALID
         PasswordStrength result1 = PasswordValidator.validate("123");
@@ -13,8 +13,34 @@ public class PasswordValidatorTest{
             System.out.println("Test Case 1 FAILED: Expected INVALID but got " + result1);
         }
 
-        // --- เขียน Test Case อื่นๆ ต่อ ---
+        // Test Case 2 : รหัสผ่านเป็น ตัวพิมพ์เล็ก, ตัวพิมพ์ใหญ่, ตัวเลข, อักษรพิเศษ อย่างใดอย่างหนึ่ง จะเป็น WEAK
+        PasswordStrength result2 = PasswordValidator.validate("baiimonn");
+        if (result2 == PasswordStrength.WEAK) {
+            System.out.println("Test Case 2 Passed: Short password is WEAK.");
+        } else {
+            System.out.println("Test Case 2 FAILED: Expected INVALID but got " + result2);
+        }
 
-        System.out.println("--------------------------------");
+        // Test Case 3 : รหัสผ่านเป็น ตัวพิมพ์เล็ก, ตัวพิมพ์ใหญ่, ตัวเลข, อักษรพิเศษ 2อย่าง จะเป็น MEDIUM
+        PasswordStrength result3 = PasswordValidator.validate("BaiiMonn");
+        if (result3 == PasswordStrength.MEDIUM) {
+            System.out.println("Test Case 3 Passed: Short password is MEDIUM.");
+        } else {
+            System.out.println("Test Case 3 FAILED: Expected INVALID but got " + result3);
+        }
+
+        // Test Case 3 : รหัสผ่านเป็น ตัวพิมพ์เล็ก, ตัวพิมพ์ใหญ่, ตัวเลข, อักษรพิเศษ มากกว่า3อย่าง จะเป็น STRONG
+        PasswordStrength result4 = PasswordValidator.validate("BaiiMon123#");
+        if (result4 == PasswordStrength.STRONG) {
+            System.out.println("Test Case 4 Passed: Short password is STRONG.");
+        } else {
+            System.out.println("Test Case 4 FAILED: Expected INVALID but got " + result4);
+        }
+
+
+
+
+
+        System.out.println("----------------------------------------------");
     }
 }
